97e8e47b692f8ade71719a50e237f7689a83974b
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-08-30 19:49:29 +0100

    small change to text

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
         keywords="personal website, mail@alfiephillips.com, portfolio"
       />
       <div className={styles.home__container}>
-        <h1 className={styles.home__container_text}>Alfie Phillips</h1>
+        <h1 className={styles.home__container_text}>In Development...</h1>
         <h3 className={styles.home__container_time}>{time}</h3>
       </div>
     </React.Fragment>
