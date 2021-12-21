bcc20b4f0e02635724f822c9a6475492911f14d9
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-12-21 13:55:10 +0000

    section

---
+++ components/Section.tsx
@@ -0,0 +1,29 @@
+import { motion } from "framer-motion";
+import { chakra, shouldForwardProp } from "@chakra-ui/react";
+
+const StyledDiv = chakra(motion.div, {
+    shouldForwardProp: (prop: any) => {
+        return shouldForwardProp(prop) || prop === "transition";
+    }
+});
+
+interface Props {
+    children: any;
+    delay: number;
+}
+
+const Section = ({ children, delay = 0 }: Props) => {
+    return (
+        <StyledDiv
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            // @ts-ignore
+            transition={{ duration: 0.8, delay }}
+            mb={6}
+        >
+            {children}
+        </StyledDiv>
+    );
+};
+
+export default Section;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,27 @@
 import type { NextPage } from "next";
-import { Container, Box, Heading, Image } from "@chakra-ui/react";
 
-import { useEffect, useState } from "react";
+import NextLink from "next/link";
+import {
+    Container,
+    Box,
+    Heading,
+    Image,
+    useColorModeValue,
+    Link
+} from "@chakra-ui/react";
+import Section from "../components/Section";
 
 const Home: NextPage = () => {
-    const [time, setTime] = useState("00:00:00 PM");
-
-    function updateTime() {
-        let current = new Date().toLocaleString("en-US", {
-            timeZone: "Europe/London"
-        });
-        setTime(current.toUpperCase().slice(-11, -1) + "M");
-        setTimeout(updateTime, 1000);
-    }
-
-    useEffect(() => {
-        updateTime();
-    });
-
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" mb={6} p={3} align="center">
-                Hi! I&apos;m a full-stack developer based in the UK!
+            <Box
+                borderRadius="lg"
+                bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+                mb={6}
+                p={3}
+                align="center"
+            >
+                Hi! I&apos;m a backend developer based in the UK.
             </Box>
 
             <Box display={{ md: "flex" }}>
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
                     <Heading as="h2" variant="page-title">
                         Alfie Jay Phillips
                     </Heading>
-                    <p>Young Entrepreneur ( Trader / Developer / Gamer )</p>
+                    <p>Young Entrepreneur ( Trader-Developer-Gamer )</p>
                 </Box>
                 <Box
                     flexShrink={0}
@@ -48,6 +48,25 @@ const Home: NextPage = () => {
                     />
                 </Box>
             </Box>
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    Work
+                </Heading>
+                <p>
+                    Alfie is a 16 year-old freelance developer based in the
+                    South-West of England with a passion for building new
+                    innovations. He aspires to become a full-time backend
+                    developer and join into the craze of Software Development:
+                    launching, managing, and designing products to solve
+                    real-life problems with the art of code. When he is not
+                    programming, he loves to spend time with his friends and try
+                    new hobbies such as trading. Currently, he is working on a
+                    product called{" "}
+                    <NextLink href="/projects/oberen">
+                        <Link>Oberen</Link>
+                    </NextLink>
+                </p>
+            </Section>
         </Container>
     );
 };
