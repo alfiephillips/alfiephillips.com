7ea390882f26b3dac2748317d99d215f11cd1d00
Alfie Phillips <17173@st-ives.cornwall.sch.uk>
2021-12-22 07:57:58 +0000

    new animation on theme button for desktop

--- components/Toggle-theme.tsx
+++ components/Toggle-theme.tsx
@@ -19,24 +19,45 @@ const ToggleTheme = ({ router }: any) => {
 
     return (
         <AnimatePresence exitBeforeEnter initial={false}>
-            <motion.div
-                style={{ display: "inline-block" }}
-                key={useColorModeValue("light", "dark")}
-                initial={{ y: -20, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{ y: 20, opacity: 0 }}
-                transition={{ duration: 0.2 }}
-                onClick={() => {
-                    playSound();
-                }}
-            >
-                <IconButton
-                    aria-label="Toggle theme"
-                    colorScheme={useColorModeValue("purple", "orange")}
-                    icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-                    onClick={toggleColorMode}
-                />
-            </motion.div>
+            {!isMobile ? (
+                <motion.div
+                    style={{ display: "inline-block" }}
+                    key={useColorModeValue("light", "dark")}
+                    initial={{ x: 400, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    exit={{ x: -1000, opacity: 0 }}
+                    transition={{ duration: 0.2 }}
+                    onClick={() => {
+                        playSound();
+                    }}
+                >
+                    <IconButton
+                        aria-label="Toggle theme"
+                        colorScheme={useColorModeValue("purple", "orange")}
+                        icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+                        onClick={toggleColorMode}
+                    />
+                </motion.div>
+            ) : (
+                <motion.div
+                    style={{ display: "inline-block" }}
+                    key={useColorModeValue("light", "dark")}
+                    initial={{ y: -20, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: 20, opacity: 0 }}
+                    transition={{ duration: 0.2 }}
+                    onClick={() => {
+                        playSound();
+                    }}
+                >
+                    <IconButton
+                        aria-label="Toggle theme"
+                        colorScheme={useColorModeValue("purple", "orange")}
+                        icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+                        onClick={toggleColorMode}
+                    />
+                </motion.div>
+            )}
         </AnimatePresence>
     );
 };
